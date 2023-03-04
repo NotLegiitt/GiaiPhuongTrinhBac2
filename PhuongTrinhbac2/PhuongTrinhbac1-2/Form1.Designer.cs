eec87e9012c7b615a72d23e7a259ca51272d5a88
NotLegiit <N0tL3giit@gmail.com>
2023-03-05 00:22:57 +0700

    first commit

---
+++ PhuongTrinhbac2/PhuongTrinhbac1-2/Form1.Designer.cs
@@ -0,0 +1,152 @@
+﻿namespace PhuongTrinhbac1_2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txbA = new System.Windows.Forms.TextBox();
+            this.txbB = new System.Windows.Forms.TextBox();
+            this.txbC = new System.Windows.Forms.TextBox();
+            this.btnGiai = new System.Windows.Forms.Button();
+            this.btnXoa = new System.Windows.Forms.Button();
+            this.txbResult = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(134, 198);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(51, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Nhap a";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(134, 249);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(51, 16);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Nhap b";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(134, 297);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(50, 16);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Nhap c";
+            // 
+            // txbA
+            // 
+            this.txbA.Location = new System.Drawing.Point(222, 195);
+            this.txbA.Name = "txbA";
+            this.txbA.Size = new System.Drawing.Size(226, 22);
+            this.txbA.TabIndex = 3;
+            // 
+            // txbB
+            // 
+            this.txbB.Location = new System.Drawing.Point(222, 246);
+            this.txbB.Name = "txbB";
+            this.txbB.Size = new System.Drawing.Size(226, 22);
+            this.txbB.TabIndex = 4;
+            // 
+            // txbC
+            // 
+            this.txbC.Location = new System.Drawing.Point(222, 297);
+            this.txbC.Name = "txbC";
+            this.txbC.Size = new System.Drawing.Size(226, 22);
+            this.txbC.TabIndex = 5;
+            // 
+            // btnGiai
+            // 
+            this.btnGiai.Location = new System.Drawing.Point(532, 173);
+            this.btnGiai.Name = "btnGiai";
+            this.btnGiai.Size = new System.Drawing.Size(143, 66);
+            this.btnGiai.TabIndex = 6;
+            this.btnGiai.Text = "Giai";
+            this.btnGiai.UseVisualStyleBackColor = true;
+            this.btnGiai.Click += new System.EventHandler(this.btnGiai_Click);
+            // 
+            // btnXoa
+            // 
+            this.btnXoa.Location = new System.Drawing.Point(532, 272);
+            this.btnXoa.Name = "btnXoa";
+            this.btnXoa.Size = new System.Drawing.Size(143, 66);
+            this.btnXoa.TabIndex = 7;
+            this.btnXoa.Text = "Xoa";
+            this.btnXoa.UseVisualStyleBackColor = true;
+            this.btnXoa.Click += new System.EventHandler(this.btnXoa_Click);
+            // 
+            // txbResult
+            // 
+            this.txbResult.Location = new System.Drawing.Point(137, 354);
+            this.txbResult.Multiline = true;
+            this.txbResult.Name = "txbResult";
+            this.txbResult.Size = new System.Drawing.Size(570, 139);
+            this.txbResult.TabIndex = 8;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 524);
+            this.Controls.Add(this.txbResult);
+            this.Controls.Add(this.btnXoa);
+            this.Controls.Add(this.btnGiai);
+            this.Controls.Add(this.txbC);
+            this.Controls.Add(this.txbB);
+            this.Controls.Add(this.txbA);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txbA;
+        private System.Windows.Forms.TextBox txbB;
+        private System.Windows.Forms.TextBox txbC;
+        private System.Windows.Forms.Button btnGiai;
+        private System.Windows.Forms.Button btnXoa;
+        private System.Windows.Forms.TextBox txbResult;
+    }
+}
+
---
+++ PhuongTrinhbac2/PhuongTrinhbac1-2/Form1.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PhuongTrinhbac1_2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnGiai_Click(object sender, EventArgs e)
+        {
+            //Lấy các giá trị các hệ số từ TextBox
+            double a = double.Parse(txbA.Text);
+            double b = double.Parse(txbB.Text);
+            double c = double.Parse(txbC.Text);
+
+            //Tính delta
+            double delta = b * b - 4 * a * c;
+
+            // Kiểm tra giá trị delta để xác định nghiệm
+            if (delta < 0)
+            {
+                txbResult.Text = "Phương trình vô nghiệm";
+            }
+            else if (delta == 0)
+            {
+                double x = -b / (2 * a);
+                txbResult.Text = $"Phương trình có nghiệm kép: x = {x}";
+            }
+            else
+            {
+                double x1 = (-b + Math.Sqrt(delta)) / (2 * a);
+                double x2 = (-b - Math.Sqrt(delta)) / (2 * a);
+                txbResult.Text = $"Phương trình có 2 nghiệm phân biệt: x1 = {x1}, x2 = {x2}";
+            }
+        }
+
+        private void btnXoa_Click(object sender, EventArgs e)
+        {
+            txbA.Text = "";
+            txbB.Text = "";
+            txbC.Text = "";
+            txbResult.Text = "";
+        }
+    }
+}
+
